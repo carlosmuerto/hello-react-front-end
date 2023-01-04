0292ac9ab25b12e8bb1f02acad7bf323f59f61e3
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-01-04 01:23:06 +0000

    Add greetings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [greeting, setGreeting] = useState([]);
+
+  const loadData = async () => {
+    const response = await axios.get('http://localhost:3000/');
+    setGreeting(response.data.text);
+    console.log(response)
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src="https://github.com/microverseinc/readme-template/blob/master/murple_logo.png?raw=true" className="App-logo" alt="logo" />
+        <h2>
+        {greeting}
+        </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
