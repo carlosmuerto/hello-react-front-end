3c8afe62fa797d84b87660207a6356c678ee6857
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-01-04 01:38:40 +0000

    update App.js

--- src/App.js
+++ src/App.js
@@ -22,14 +22,6 @@ function App() {
         <h2>
         {greeting}
         </h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
